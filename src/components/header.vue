ed17225cc7c7d6e5bd0b7b360474e105221ea478
Dev <dev@Dev.local>
2018-06-15 13:00:10 +0200

    get plans on header

--- src/components/header.vue
+++ src/components/header.vue
@@ -9,7 +9,7 @@
 
         <b-navbar-nav>
           <b-nav-item href="#">OVERVIEW</b-nav-item>
-          <b-nav-item href="#">ASSUMPTION</b-nav-item>
+          <b-nav-item href="#">ASSUMPTIONS</b-nav-item>
           <b-nav-item href="#">TIMELINE</b-nav-item>
           <b-nav-item href="#">WHAT IF?</b-nav-item>
           <b-nav-item href="#">TOOLS / INSIGHTS</b-nav-item>
@@ -17,7 +17,8 @@
 
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto mr-4">
-          <b-dropdown id="ddown1" right size="sm" text="Plan: Until we are 80">
+          <b-dropdown id="ddown1" right size="sm" :text="selectedPlanText">
+            <b-dropdown-item-button v-for="plan in plans" :key="plan.id" @click="selectPlan(plan)">{{plan.description}}</b-dropdown-item-button>
           </b-dropdown>
           <div class="mx-4 spacing-divider"></div>
           <b-nav-item-dropdown right no-caret>
@@ -35,12 +36,33 @@
 
 <script>
 import {AUTH_LOGOUT} from '@/store/actions/auth'
+import axios from 'axios'
 
 export default {
   name: 'vheader',
+  data () {
+    return {
+      plans: [],
+      selectedPlan: null
+    }
+  },
+  created () {
+    axios.get('https://api.livsplan.se/api/v1/plans')
+      .then(resp => (
+        this.plans = resp.data.data
+      ))
+      .catch(error => console.log(error))
+  },
   computed: {
     isAuthenticated () {
       return this.$store.getters.isAuthenticated
+    },
+    selectedPlanText () {
+      if (this.selectedPlan) {
+        return this.selectedPlan.description
+      } else {
+        return 'Select Plan'
+      }
     }
   },
   methods: {
@@ -48,7 +70,11 @@ export default {
       this.$store.dispatch(AUTH_LOGOUT).then(() => {
         this.$router.push('/login')
       })
+    },
+    selectPlan (plan) {
+      this.selectedPlan = plan
     }
+
   }
 }
 </script>

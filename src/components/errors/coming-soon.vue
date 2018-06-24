c82790e46a8a230ed6b88a928b70a24f7fca7bcf
Dev <dev@Dev.local>
2018-06-24 20:08:55 +0200

    coming soon page

---
+++ src/components/errors/coming-soon.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="comingSoon bg-light">
+    <h1 class="display-3">Comming Soon</h1>
+    <h2>
+      Our website is currently undergoing scheduled maintance.<br>we should be back shortly. Thanks for your patience.
+    </h2>
+    <router-link :to="{name: 'login'}">Go to Login</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'comingSoon'
+}
+</script>
+
+<style lang="scss">
+  .comingSoon {
+    height: 100vh;
+    padding-top: 70px;
+
+    .display-3 {
+      font-weight: 500;
+    }
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import login from '@/components/auth/login'
 import notFound from '@/components/errors/404'
+import comingSoon from '@/components/errors/coming-soon'
 import assumptions from '@/components/assumptions'
 import expense from '@/components/assumptions/expense'
 
@@ -23,6 +24,11 @@ export default new Router({
       path: '/error-404',
       name: 'notFound',
       component: notFound
+    },
+    {
+      path: '/coming-soon',
+      name: 'comingSoon',
+      component: comingSoon
     },
     {
       path: '/assumptions',

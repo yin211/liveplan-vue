023e5c7730bc7fe07e93bfab3bb08632469d26f4
Dev <dev@Dev.local>
2018-06-27 06:44:37 +0200

    refactor expense page

--- src/components/assumptions/expense.vue
+++ src/components/assumptions/expense.vue
@@ -1,154 +1,156 @@
 <template>
   <div class="expense bg-light">
     <b-breadcrumb :items="items" class="p-0"/>
-    <b-container fluid class="px-0 text-left">
-      <h1 class="text-regular">{{expense.name}}</h1>
-      <b-row class="pt-2">
-          <b-col md="6">
-            <p class="text-regular" style="margin-bottom: 10px">Explanation</p>
-            <p class="text-gray">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-          </b-col>
-          <b-col md="6">
-            <p class="text-regular" style="margin-bottom: 10px">Description</p>
-            <p class="text-gray">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-          </b-col>
-      </b-row>
-    </b-container>
-    <section>
-      <b-card style="margin-top: 40px;">
-        <b-form class="text-left">
-          <b-container fluid class="px-0">
-            <b-row>
-              <b-col lg="5">
-                <b-row class="no-gutters">
-                  <b-col sm="4" class="d-flex">
-                    <div class="d-flex align-items-center">
-                      <i class="fa fa-car car-icon"></i>
-                      <div class="d-flex flex-column ml-3">
-                        <span class="text-gray">Category</span>
-                        <span class="text-regular">Car</span>
-                      </div>
-                    </div>
-                    <div class="rect-spacer d-none d-lg-block" style="margin: -24px 0 -20px 30px;"></div>
-                  </b-col>
-                  <b-col sm="4">
-                    <span class="text-regular">Start - End Year</span>
-                    <div class="d-flex element-spacer">
-                      <b-form-input v-model="expense.start_year" type="number" size="sm"></b-form-input>
-                      <span class="date-spacer">-</span>
-                      <b-form-input v-model="expense.end_year" type="number" size="sm"></b-form-input>
-                    </div>
-                  </b-col>
-                  <b-col sm="4" class="pl-sm-4">
-                    <span class="text-regular">Amount Per Month</span>
-                    <b-input-group size="sm" append="SEK" class="element-spacer">
+    <h1 class="text-regular text-left">{{expense.name}}</h1>
+    <section class="chart-container depth-2">
+      <barchart v-if="cashflow.length" :dataArray="cashflow"></barchart>
+    </section>
+    <b-card no-body class="expense-tabs-card" style="margin-top: -170px">
+      <b-tabs pills card>
+        <b-tab title="Auto Calculation" :title-link-class="'expense-tab'" active>
+          <div class="tab-padding-top"></div>
+          <b-form>
+            <b-container fluid>
+              <b-row class="text-left mx-auto">
+                <b-col lg="2">
+                  <span class="text-regular">Start - End Year</span>
+                  <div class="d-flex element-spacer">
+                    <b-form-input v-model="expense.start_year" type="number" size="sm"></b-form-input>
+                    <span class="date-spacer">-</span>
+                    <b-form-input v-model="expense.end_year" type="number" size="sm"></b-form-input>
+                  </div>
+                </b-col>
+                <b-col lg="5">
+                  <span class="text-regular">Amount Per Month</span>
+                  <div class="d-flex">
+                    <b-input-group size="sm" append="SEK" class="element-spacer amount-per-month">
                       <b-form-input v-model="expense.initial_amount" type="number"></b-form-input>
                     </b-input-group>
-                  </b-col>
-                </b-row>
-              </b-col>
-              <b-col lg="7">
-                <b-row>
-                  <b-col cols="12" md="8">
-                    <b-row>
-                      <b-col sm="5" class="d-flex">
-                        <div class="rect-spacer d-none d-lg-block" style="margin: -24px 0px -20px 0px;"></div>
-                        <div class="d-flex flex-column">
-                          <span class="text-regular">Period</span>
-                          <b-form-select v-model="expense.initial_amount_period" :options="periodOptions" size="sm" class="element-spacer"/>
-                        </div>
-                      </b-col>
-                      <b-col sm="4">
-                        <span class="text-regular">Annual Growth Rate</span>
-                        <b-input-group size="sm" append="%" class="w-75 element-spacer">
-                          <b-form-input v-model="expense.annual_increase_percentage" type="number"></b-form-input>
-                        </b-input-group>
-                      </b-col>
-                      <b-col sm="3">
-                        <span class="text-regular">Inflation</span>
-                        <b-input-group size="sm" append="%" class="element-spacer">
-                          <b-form-input v-model="expense.inflation_rate" type="number"></b-form-input>
-                        </b-input-group>
-                      </b-col>
-                    </b-row>
-                  </b-col>
-                  <b-col cols="12" md="4" class="d-flex">
-                    <div class="rect-spacer d-none d-lg-block" style="margin: -24px 0px -20px 0px;"></div>
-                    <button class='btn plain-btn text-regular'>
-                      <i class="fa fa-pencil mr-2 text-primary" style="font-size: 14px"></i> Edit
-                    </button>
-                    <button class='btn plain-btn text-regular'>
-                      <i class="fa fa-trash mr-2 text-danger" style="font-size: 14px"></i> Delete
-                    </button>
-                  </b-col>
-                </b-row>
-              </b-col>
-            </b-row>
-          </b-container>
-        </b-form>
-      </b-card>
-    </section>
-    <section class="chart-container depth-3">
-      <barchart v-if="cashflow.length"
-                  :dataArray="cashflow"
-      ></barchart>
-    </section>
-    <section class="text-left">
-      <b-container fluid class="px-0">
-        <h2>Data Table</h2>
-        <div class="table-container text-regular">
-          <b-table show-empty
-                  stacked="md"
-                  :items="expense.expense_amounts"
-                  :fields="fields"
-                  :current-page="currentPage"
-                  :per-page="perPage"
-                  :filter="filter"
-                  :sort-by.sync="sortBy"
-                  :sort-desc.sync="sortDesc"
-                  :sort-direction="sortDirection"
-                  @filtered="onFiltered"
-                  hover
-                  striped
-          >
-            <template slot="top-row" slot-scope="data">
-              <td colspan="3"><b-form-input v-model="filter" size="sm" placeholder="Type to Search" /></td>
-            </template>
-            <template slot="amount" slot-scope="data">
-              {{data.item.amount}} SEK
-            </template>
-            <template slot="actions" slot-scope="row">
-              <button class='btn plain-btn text-regular'>
-                <i class="fa fa-pencil mr-2 text-primary"></i> Edit
-              </button>
-              <button class='btn plain-btn text-regular'>
-                <i class="fa fa-times mr-2 text-danger"></i> Delete
-              </button>
-            </template>
-          </b-table>
-          <div class="d-flex  flex-column flex-md-row justify-content-between">
-            <div class="d-flex">
-              <div v-if="totalRows > 0" class="d-flex align-items-center">
-                <span class="text-gray">Showing {{(currentPage - 1) * perPage + 1}} to {{endRecord}} of {{totalRows}}</span>
+                    <b-input-group size="sm" class="element-spacer">
+                      <b-form-input v-model="expense.initial_amount" min="1" max="10000" type="range"></b-form-input>
+                    </b-input-group>
+                  </div>
+                </b-col>
+                <b-col lg="5">
+                  <span class="text-regular">Annual Growth Rate</span>
+                  <div class="d-flex">
+                    <b-input-group size="sm" append="%" class="element-spacer annaul-growth-rate">
+                      <b-form-input v-model="expense.annual_increase_percentage" type="number"></b-form-input>
+                    </b-input-group>
+                    <b-input-group size="sm" class="element-spacer">
+                      <b-form-input v-model="expense.annual_increase_percentage" min="1" max="100" type="range"></b-form-input>
+                    </b-input-group>
+                  </div>
+                </b-col>
+              </b-row>
+            </b-container>
+          </b-form>
+        </b-tab>
+        <b-tab title="Custom Values" :title-link-class="'expense-tab'">
+          <div class="tab-padding-top"></div>
+          <div class="table-container text-regular text-left">
+            <b-table show-empty
+                    stacked="md"
+                    :items="expense.expense_amounts"
+                    :fields="fields"
+                    :current-page="currentPage"
+                    :per-page="perPage"
+                    :filter="filter"
+                    :sort-by.sync="sortBy"
+                    :sort-desc.sync="sortDesc"
+                    :sort-direction="sortDirection"
+                    @filtered="onFiltered"
+                    hover
+            >
+              <template slot="top-row" slot-scope="data">
+                <td colspan="3"><b-form-input v-model="filter" size="sm" placeholder="Type to Search" /></td>
+              </template>
+              <template slot="amount" slot-scope="data">
+                {{data.item.amount}} SEK
+              </template>
+              <template slot="actions" slot-scope="row">
+                <button class='btn plain-btn text-regular'>
+                  <i class="fa fa-pencil mr-2 text-primary"></i> Edit
+                </button>
+                <button class='btn plain-btn text-regular'>
+                  <i class="fa fa-times mr-2 text-danger"></i> Delete
+                </button>
+              </template>
+            </b-table>
+            <div class="d-flex  flex-column flex-md-row justify-content-between">
+              <div class="d-flex">
+                <div v-if="totalRows > 0" class="d-flex align-items-center">
+                  <span class="text-gray">Showing {{(currentPage - 1) * perPage + 1}} to {{endRecord}} of {{totalRows}}</span>
+                </div>
+                <div v-if="totalRows > 0" class="space-divider"></div>
+                <div class="d-flex align-items-center">
+                  <span> show per page: </span>
+                  <b-form-select :options="pageOptions" v-model="perPage" class="selectPerPage" />
+                </div>
               </div>
-              <div v-if="totalRows > 0" class="space-divider"></div>
               <div class="d-flex align-items-center">
-                <span> show per page: </span>
-                <b-form-select :options="pageOptions" v-model="perPage" class="selectPerPage" />
+                <b-pagination :total-rows="totalRows" :per-page="perPage" v-model="currentPage" class="my-0"/>
               </div>
-            </div>
-            <div class="d-flex align-items-center">
-              <b-pagination :total-rows="totalRows" :per-page="perPage" v-model="currentPage" class="my-0"/>
             </div>
           </div>
-        </div>
-      </b-container>
-    </section>
+        </b-tab>
+      </b-tabs>
+    </b-card>
+    <b-card style="margin-top: 34px; margin-bottom: 240px;">
+      <b-container fluid>
+        <b-row class="text-left">
+          <b-col md="9" xl="7">
+            <b-row>
+              <b-col lg="6" class="d-flex align-items-center justify-content-between">
+                <div class="d-flex align-items-center">
+                  <i class="fa fa-car car-icon"></i>
+                  <div class="d-flex flex-column ml-3">
+                    <span class="text-gray">Name</span>
+                    <span class="text-regular">Car Rental</span>
+                  </div>
+                </div>
 
+                <div class="d-flex flex-column">
+                  <span class="text-gray">Category</span>
+                  <span class="text-regular">Assets</span>
+                </div>
+
+                <div class="d-flex flex-column">
+                  <span class="text-gray">Sub-category</span>
+                  <span class="text-regular">Assets</span>
+                </div>
+              </b-col>
+              <b-col lg="6" class="d-flex align-items-center justify-content-between">
+                <div class="d-flex flex-column ml-lg-3">
+                  <span class="text-gray">Inflation</span>
+                  <span class="text-regular">{{expense.inflation_rate}} %</span>
+                </div>
+
+                <div class="d-flex flex-column">
+                  <span class="text-gray">Period</span>
+                  <span class="text-regular">{{expense.initial_amount_period}}</span>
+                </div>
+
+                <div class="d-flex flex-column">
+                  <span class="text-gray">Person</span>
+                  <span class="text-regular">Job Bolmeson (me)</span>
+                </div>
+
+                <div class="d-flex flex-column">
+                  <span class="text-gray">Tax</span>
+                  <span class="text-regular">10%</span>
+                </div>
+              </b-col>
+            </b-row>
+          </b-col>
+          <b-col md="3" xl="5" class="text-center text-md-right">
+              <button class='btn plain-btn text-regular'>
+                <i class="fa fa-pencil mr-2 text-primary" style="font-size: 14px"></i> Edit
+              </button>
+          </b-col>
+        </b-row>
+      </b-container>
+    </b-card>
   </div>
 </template>
 
@@ -244,13 +246,59 @@ export default {
       margin-bottom: 120px;
     }
 
-    .element-spacer {
-      margin-top: 8px;
+    .breadcrumb {
+      background: transparent;
+      margin-bottom: 24px;
     }
 
-    .rect-spacer {
-      width: 50px;
-      background: linear-gradient(270deg, #FFFFFF 0%, #F8F8F8 100%);
+    .chart-container {
+      margin-left: calc(50% - 50vw);
+      margin-right: calc(50% - 50vw);
+      margin-top: 38px;
+    }
+
+    .expense-tabs-card {
+      .card-header {
+        padding: 34px 30px;
+        background: white;
+        border-bottom: none;
+
+        .expense-tab {
+          font-size: 24px;
+          line-height: 32px;
+          color: #84888F;
+          &.active {
+            color: #434343;
+            font-weight: 600;
+            background-color: transparent;
+          }
+        }
+      }
+
+      .card-body {
+        padding: 0px 0px 28px 0px;
+
+        .tab-padding-top {
+          height: 28px;
+          background: linear-gradient(270deg, #FFFFFF 0%, #F8F8F8 100%);
+        }
+
+        form {
+          .amount-per-month {
+            width: 250px;
+            margin-right: 20px;
+          }
+          .annaul-growth-rate {
+            width: 120px;
+            margin-right: 20px;
+          }
+        }
+      }
+
+    }
+
+    .element-spacer {
+      margin-top: 6px;
     }
 
     .date-spacer {
@@ -266,22 +314,9 @@ export default {
       margin: auto 24px;
     }
 
-    .breadcrumb {
-      background: transparent;
-    }
-
-    .card-body {
-      padding: 24px 20px 20px;
-    }
-
     .card span {
       font-size: 14px;
       line-height: 20px;
-    }
-
-    h2 {
-      font-weight: 600;
-      color: #434343;
     }
 
     .car-icon {
@@ -303,15 +338,8 @@ export default {
         margin: 0; /* <-- Apparently some margin are still there even though it's hidden */
     }
 
-
-    .chart-container {
-      margin-left: calc(50% - 50vw);
-      margin-right: calc(50% - 50vw);
-      margin-top: -180px;
-    }
-
     .table-container {
-      margin-top: 20px;
+      padding: 0 20px;
       font-size: 14px;
 
       table {
@@ -321,6 +349,7 @@ export default {
           background: white;
           box-shadow: 9px 8px 16px 0 rgba(0,0,0,0.07);
           outline: none;
+          border: none;
         }
 
         tbody {
@@ -329,9 +358,9 @@ export default {
             border-bottom-width: 2px;
             border-bottom-color: #CACACA;
 
-            &:nth-of-type(odd) {
-              background-color: #F1F2F8 !important;
-            }
+            // &:nth-of-type(odd) {
+            //   background-color: #F1F2F8 !important;
+            // }
 
             &:hover {
               background: #FAFBFC !important;
@@ -361,13 +390,11 @@ export default {
       ul[aria-label="Pagination"] {
         .page-link {
           background: transparent;
-          width: 30px;
-          height: 30px;
           font-size: 13px;
           line-height: 13px;
           border: none;
           color: #4A4A4A;
-
+          display: inline;
         }
 
         .page-item.active .page-link {

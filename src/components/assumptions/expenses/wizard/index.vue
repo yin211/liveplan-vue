e52efd024548dce7c2602eae250052a1a1dab04a
Dev <dev@Dev.local>
2018-08-22 17:31:25 +0200

    change step1

--- src/components/assumptions/expenses/wizard/index.vue
+++ src/components/assumptions/expenses/wizard/index.vue
@@ -2,12 +2,9 @@
   <div class="addExpense bg-light">
     <div class="step-container">
       <div class="stepper mx-auto bg-light text-regular">
-        <form-wizard @on-complete="onComplete" :title="'Create New Expense'" :subtitle="''" :stepSize="'xs'" :color="'#525670'" ref="step">
-          <tab-content title="Type" icon="fa fa-check">
-            <step1 @on-validate="mergePartialModels"></step1>
-          </tab-content>
+        <form-wizard :title="'Create New Expense'" :subtitle="''" :stepSize="'xs'" :color="'#525670'" ref="step">
           <tab-content title="Basic Details" icon="fa fa-check">
-            <step2 @on-validate="mergePartialModels"></step2>
+            <step1 @on-validate="mergePartialModels"></step1>
           </tab-content>
           <tab-content title="Timing" icon="fa fa-check">
             My third tab content
@@ -16,12 +13,15 @@
             My fourth tab content
           </tab-content>
           <template slot="footer" slot-scope="props">
-            <div class=wizard-footer-left>
-              <wizard-button  v-if="props.activeTabIndex > 0 && !props.isLastStep" @click.native="props.prevTab()" :style="props.fillButtonStyle">Previous</wizard-button>
-            </div>
-            <div class="wizard-footer-right">
-              <wizard-button v-if="!props.isLastStep && props.activeTabIndex != 0" @click.native="props.nextTab()" class="wizard-footer-right" :style="props.fillButtonStyle">Next</wizard-button>
-              <wizard-button v-if="props.isLastStep" @click.native="alert('Done')" class="wizard-footer-right finish-button" :style="props.fillButtonStyle">{{props.isLastStep ? 'Done' : 'Next'}}</wizard-button>
+            <div :class="{ hasSpace: props.activeTabIndex == 1 }">
+              <div class=wizard-footer-left>
+                <wizard-button  v-if="props.activeTabIndex > 0 && !props.isLastStep" @click.native="props.prevTab()" :style="props.fillButtonStyle">Back</wizard-button>
+              </div>
+              <div class="line" v-if="props.activeTabIndex == 1"></div>
+              <div class="wizard-footer-right">
+                <wizard-button v-if="!props.isLastStep" @click.native="props.nextTab()" class="wizard-footer-right" :style="props.fillButtonStyle">Next</wizard-button>
+                <wizard-button v-else @click.native="onComplete" class="wizard-footer-right finish-button" :style="props.fillButtonStyle">{{props.isLastStep ? 'Done' : 'Next'}}</wizard-button>
+              </div>
             </div>
           </template>
         </form-wizard>
@@ -34,7 +34,6 @@
 <script>
 // import { required, minLength, between } from 'vuelidate/lib/validators'
 import step1 from './steps/step1'
-import step2 from './steps/step2'
 
 export default {
   name: 'addExpense',
@@ -57,8 +56,7 @@ export default {
     }
   },
   components: {
-    step1,
-    step2
+    step1
   }
 }
 </script>
--- src/components/assumptions/expenses/wizard/steps/step1.vue
+++ src/components/assumptions/expenses/wizard/steps/step1.vue
@@ -1,60 +1,105 @@
 <template>
-  <div class="step1">
-    <b-card-group deck>
-      <b-card class="depth-1">
-        <div class="card-body-top"></div>
-        <div class="card-body-bottom">
-          <p class="card-text">
-            Housing costs
-          </p>
-          <b-button :size="'sm'" variant="primary" class="mt-3"  @click="choose_type()">Choose</b-button>
-        </div>
-      </b-card>
-      <b-card class="depth-2">
-        <div class="card-body-top"></div>
-        <div class="card-body-bottom">
-          <p class="card-text">
-            Living costs
-          </p>
-          <b-button :size="'sm'" variant="primary" class="mt-3" @click="choose_type()">Choose</b-button>
-        </div>
-      </b-card>
-      <b-card class="depth-1">
-        <div class="card-body-top"></div>
-        <div class="card-body-bottom">
-          <p class="card-text">
-            Custom
-          </p>
-          <b-button :size="'sm'" variant="primary" class="mt-3" @click="choose_type()">Choose</b-button>
-        </div>
-      </b-card>
-      <b-card class="depth-1">
-        <div class="card-body-top"></div>
-        <div class="card-body-bottom">
-          <b-form-select v-model="selected" :options="options" size="sm" style="margin-top: -10px; margin-bottom: 10px;"/>
-          <b-button :size="'sm'" variant="primary" class="mt-3" @click="choose_type()">Choose</b-button>
-        </div>
-      </b-card>
-    </b-card-group>
+  <div class="step1 d-flex">
+    <div class="detail-text text-left">
+      <p class="font-weight-medium">Setting up the Basic Details</p>
+      <span class="text-gray">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </span>
+    </div>
+    <div class="space-line"></div>
+    <b-container>
+      <b-row class="mb-3">
+        <b-col sm="3" class="d-flex justify-content-end mt-3"><label :for="'nameInput'">Name</label></b-col>
+        <b-col sm="9">
+          <b-form-input id="nameInput"  v-model="name" :placeholder="'Write here'" class="border-0"></b-form-input>
+        </b-col>
+      </b-row>
+      <b-row class="mb-3">
+        <b-col sm="3" class="d-flex mt-3 justify-content-end"><label :for="'subtype-select'">Subtype</label></b-col>
+        <b-col sm="9">
+          <b-form-select :options="subtypeOptions" :value-field="'subtype_id'" :text-field="'subtype'" v-model="subtype_id" :id="'subtype-select'" class="border-0"/>
+        </b-col>
+      </b-row>
+      <b-row class="mb-3">
+        <b-col sm="3" class="d-flex justify-content-end mt-3"><label :for="'descriptionInput'">Description</label></b-col>
+        <b-col sm="9">
+          <b-form-textarea id="descriptionInput"  v-model="description" :placeholder="'Write here'" class="border-0" :rows="3" :max-rows="6"></b-form-textarea>
+        </b-col>
+      </b-row>
+      <b-row class="mb-3">
+        <b-col sm="3" class="d-flex justify-content-end mt-3"><label :for="'amountInput'">Amount</label></b-col>
+        <b-col sm="9" class="d-flex">
+          <vue-numeric currency="SEK" currency-symbol-position="suffix" thousand-separator=" " v-model="amount" id="amountInput" class="form-control border-0 mr-3" :min="0"></vue-numeric>
+          <b-form-input v-model="amount" min="0" max="10000" step="500" class="slider" type="range"></b-form-input>
+        </b-col>
+      </b-row>
+      <b-row class="mb-3">
+        <b-col sm="3" class="d-flex mt-3 justify-content-end"><label :for="'period-select'">Period</label></b-col>
+        <b-col sm="9">
+          <b-form-select :options="periodOptions" v-model="amount_recurrence" :id="'period-select'" class="border-0"/>
+        </b-col>
+      </b-row>
+      <b-row class="mb-3">
+        <b-col sm="3" class="d-flex mt-3 justify-content-end"><label :for="'category-select'">Category</label></b-col>
+        <b-col sm="9">
+          <b-form-select :options="categoryOptions" :value-field="'id'" :text-field="'name'" v-model="category_id" :id="'category-select'" class="border-0" />
+        </b-col>
+      </b-row>
+      <b-row class="mb-3">
+        <b-col sm="3" class="d-flex mt-3 justify-content-end"><label :for="'person-select'">Person</label></b-col>
+        <b-col sm="9">
+          <b-form-select :options="personOptions" :value-field="'id'" :text-field="'name'" v-model="person_id" :id="'person-select'" class="border-0" />
+        </b-col>
+      </b-row>
+    </b-container>
+
   </div>
 
 </template>
 
 <script>
+import axios from 'axios'
 
 export default {
   data () {
     return {
-      selected: null,
-      type: null,
-      options: [
-        { value: null, text: 'Other: Please select' }
-      ]
+      name: null,
+      subtypeOptions: [],
+      subtype_id: null,
+      description: null,
+      periodOptions: [
+        { value: null, text: 'Please select', disabled: true },
+        { value: 'monthly', text: 'Monthly', display: 'month' },
+        { value: 'daily', text: 'Daily', display: 'day' },
+        { value: 'weekly', text: 'Weekly', display: 'week' },
+        { value: 'quarterly', text: 'Quarterly', display: 'quarter' },
+        { value: 'semiannually', text: 'Semiannually', display: 'half-year' },
+        { value: 'annually', text: 'Annually', display: 'year' },
+        { value: 'onetime', text: 'One time', dispay: 'one time' }
+      ],
+      amount_recurrence: null,
+      category_id: null,
+      categoryOptions: [
+        { id: null, name: 'Please select', disabled: true }
+      ],
+      personOptions: [
+        { id: null, name: 'Please select', disabled: true }
+      ],
+      person_id: null,
+      amount: 0
     }
   },
-  methods: {
-    choose_type () {
-      this.$emit('on-validate', this.type, true)
+  async mounted () {
+    try {
+      let response = await axios.get(`${process.env.ROOT_API}/expenses/subtypes`)
+      this.subtypeOptions = response.data
+      response = await axios.get(`${process.env.ROOT_API}/categories`)
+      this.categoryOptions = [...this.categoryOptions, ...response.data.data]
+      response = await axios.get(`${process.env.ROOT_API}/persons`)
+      this.personOptions = [...this.personOptions, ...response.data.data]
+    } catch (err) {
+      console.log(err)
     }
   }
 }
@@ -62,43 +107,19 @@ export default {
 
 <style lang="scss">
   .step1 {
-    .card-deck {
-      margin-left: -36px;
-      margin-right: -36px;
 
-      .card {
-        border: none;
+    .detail-text {
+      margin-top: 164px;
+      min-width: 436px;
+      max-width: 436px;
+    }
 
-        .card-body {
-          padding: 0;
-          display: flex;
-          flex-direction: column;
-
-          .card-body-top {
-            height: 164px;
-            opacity: 0.29;
-            background-color: #525670;
-          }
-
-          .card-body-bottom {
-            padding: 28px 18px 34px;
-
-            .card-text {
-              color: #232B36;
-              font-size: 18px;
-              font-weight: 500;
-              line-height: 24px;
-            }
-
-            .btn {
-              border-radius: 28px;
-              padding-left: 14px;
-              padding-right: 14px;
-              font-weight: 500;
-            }
-          }
-        }
-      }
+    .space-line {
+      height: 533px;
+      width: 0;
+      border: 1px solid #D7D7D7;
+      margin-left: 130px;
+      margin-right: 130px;
     }
   }
 </style>

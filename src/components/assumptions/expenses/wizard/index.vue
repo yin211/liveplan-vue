4dfdb90f2d31f716c6489fc447b508e6be562faa
Dev <dev@Dev.local>
2018-08-22 19:05:59 +0200

    step2

--- src/components/assumptions/expenses/wizard/index.vue
+++ src/components/assumptions/expenses/wizard/index.vue
@@ -7,7 +7,7 @@
             <step1 @on-validate="mergePartialModels"></step1>
           </tab-content>
           <tab-content title="Timing" icon="fa fa-check">
-            My third tab content
+            <step2 @on-validate="mergePartialModels"></step2>
           </tab-content>
           <tab-content title="Advanced" icon="fa fa-check">
             My fourth tab content
@@ -34,6 +34,7 @@
 <script>
 // import { required, minLength, between } from 'vuelidate/lib/validators'
 import step1 from './steps/step1'
+import step2 from './steps/step2'
 
 export default {
   name: 'addExpense',
@@ -56,7 +57,8 @@ export default {
     }
   },
   components: {
-    step1
+    step1,
+    step2
   }
 }
 </script>
---
+++ src/components/assumptions/expenses/wizard/steps/step2.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="step2 d-flex">
+    <div class="detail-text text-left">
+      <p class="font-weight-medium">Time of your income</p>
+      <span class="text-gray">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </span>
+    </div>
+    <div class="space-line"></div>
+    <b-container>
+      <b-row class="mb-3">
+        <b-col sm="3" class="d-flex justify-content-end mt-3 pr-0"><label :for="'start_year'">Starts at</label></b-col>
+        <b-col sm="9">
+          <date-picker id="start_year" v-model="start_year" lang="en" type="year" format="YYYY" >
+            <template slot="calendar-icon">
+              <i class="flaticon stroke calendar-2 text-primary"></i>
+            </template>
+          </date-picker>
+        </b-col>
+      </b-row>
+      <b-row class="mb-3">
+        <b-col sm="3" class="d-flex justify-content-end mt-3 pr-0"><label :for="'end_year'">Ends at</label></b-col>
+        <b-col sm="9">
+          <date-picker id="end_year" v-model="end_year" lang="en" type="year" format="YYYY" >
+            <template slot="calendar-icon">
+              <i class="flaticon stroke calendar-2 text-primary"></i>
+            </template>
+          </date-picker>
+        </b-col>
+      </b-row>
+    </b-container>
+
+  </div>
+
+</template>
+
+<script>
+import DatePicker from 'vue2-datepicker'
+
+export default {
+  components: { DatePicker },
+  data () {
+    return {
+      start_year: null,
+      end_year: null
+    }
+  },
+  async mounted () {
+
+  }
+}
+</script>
+
+<style lang="scss">
+  .step2 {
+    .detail-text {
+      margin-top: 42px;
+      min-width: 436px;
+      max-width: 436px;
+    }
+
+    .space-line {
+      height: 533px;
+      width: 0;
+      border: 1px solid #D7D7D7;
+      margin-left: 130px;
+      margin-right: 130px;
+    }
+
+  }
+</style>

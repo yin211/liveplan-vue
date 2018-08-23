a3500ceedc34b49125379c5db051293185be924c
Dev <dev@Dev.local>
2018-08-23 13:16:38 +0200

    step3

--- src/components/assumptions/expenses/wizard/index.vue
+++ src/components/assumptions/expenses/wizard/index.vue
@@ -10,17 +10,17 @@
             <step2 @on-validate="mergePartialModels"></step2>
           </tab-content>
           <tab-content title="Advanced" icon="fa fa-check">
-            My fourth tab content
+            <step3 @on-validate="mergePartialModels"></step3>
           </tab-content>
           <template slot="footer" slot-scope="props">
-            <div :class="{ hasSpace: props.activeTabIndex == 1 }">
+            <div :class="{ hasSpace: props.activeTabIndex != 0 }">
               <div class=wizard-footer-left>
-                <wizard-button  v-if="props.activeTabIndex > 0 && !props.isLastStep" @click.native="props.prevTab()" :style="props.fillButtonStyle">Back</wizard-button>
+                <wizard-button  v-if="props.activeTabIndex > 0" @click.native="props.prevTab()" :style="props.fillButtonStyle">Back</wizard-button>
               </div>
-              <div class="line" v-if="props.activeTabIndex == 1"></div>
+              <div class="line" v-if="props.activeTabIndex != 0"></div>
               <div class="wizard-footer-right">
                 <wizard-button v-if="!props.isLastStep" @click.native="props.nextTab()" class="wizard-footer-right" :style="props.fillButtonStyle">Next</wizard-button>
-                <wizard-button v-else @click.native="onComplete" class="wizard-footer-right finish-button" :style="props.fillButtonStyle">{{props.isLastStep ? 'Done' : 'Next'}}</wizard-button>
+                <wizard-button v-else @click.native="onComplete" class="wizard-footer-right finish-button" :style="props.fillButtonStyle">Next</wizard-button>
               </div>
             </div>
           </template>
@@ -35,6 +35,7 @@
 // import { required, minLength, between } from 'vuelidate/lib/validators'
 import step1 from './steps/step1'
 import step2 from './steps/step2'
+import step3 from './steps/step3'
 
 export default {
   name: 'addExpense',
@@ -58,7 +59,8 @@ export default {
   },
   components: {
     step1,
-    step2
+    step2,
+    step3
   }
 }
 </script>
---
+++ src/components/assumptions/expenses/wizard/steps/step3.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="step3 d-flex">
+    <div class="detail-text text-left">
+      <p class="font-weight-medium">Couple Advanced Settings</p>
+      <span class="text-gray">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </span>
+    </div>
+    <div class="space-line"></div>
+    <b-container class="p-0">
+      <b-row class="mb-3">
+        <b-col sm="3" class="d-flex justify-content-end mt-3"><label :for="'annual-growth-rate'">Growth Rate</label></b-col>
+        <b-col sm="9" class="d-flex">
+          <vue-numeric currency="%" currency-symbol-position="suffix" v-model="annual_increase_percentage" class="form-control border-0 mr-3" id="annual-growth-rate" :min="0" :max="10" style="width: 100px;"></vue-numeric>
+          <b-form-input v-model="annual_increase_percentage" min="0" max="10" class="slider" type="range"></b-form-input>
+        </b-col>
+      </b-row>
+      <b-row class="mb-3">
+        <b-col sm="3" class="d-flex justify-content-end mt-3"><label :for="'inflation-input'">Inflation</label></b-col>
+        <b-col sm="9" class="d-flex">
+          <vue-numeric currency="%" currency-symbol-position="suffix" v-model="inflation_rate" class="form-control border-0 mr-3" id="inflation-input" :min="0" :max="10" style="width: 100px;"></vue-numeric>
+          <b-form-input v-model="inflation_rate" min="0" max="10" class="slider" type="range"></b-form-input>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col sm="3" class="d-flex mt-3 justify-content-end"><label :for="'currency-select'">Currency</label></b-col>
+        <b-col sm="9">
+          <b-form-select :options="currencyOptions" :value-field="'id'" :text-field="'name'" v-model="currency_id" :id="'currency-select'" class="border-0"/>
+        </b-col>
+      </b-row>
+    </b-container>
+
+  </div>
+
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data () {
+    return {
+      currencyOptions: [],
+      currency_id: null,
+      annual_increase_percentage: 0,
+      inflation_rate: 0
+    }
+  },
+  async mounted () {
+    try {
+      let response = await axios.get(`${process.env.ROOT_API}/currencies`)
+      this.currencyOptions = response.data.data
+    } catch (err) {
+      console.log(err)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .step3 {
+    .detail-text {
+      margin-top: 42px;
+      min-width: 436px;
+      max-width: 436px;
+    }
+
+    .space-line {
+      height: 175px;
+      width: 0;
+      border: 1px solid #D7D7D7;
+      margin-left: 130px;
+      margin-right: 130px;
+    }
+
+  }
+</style>

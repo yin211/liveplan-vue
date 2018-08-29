bccb29a21c64ff7bb3ad821c83127847dfb5674f
Giorgi Ghviniashvili <mr.g.ghv@gmail.com>
2018-08-29 21:55:48 +0400

    tooltip sum

--- src/components/assumptions/expenses/index.vue
+++ src/components/assumptions/expenses/index.vue
@@ -18,13 +18,13 @@
       <div class="chart-container">
         <stackedBarChart v-if="isStackedBarChart && stackedData && stackedData.length && planStartYear && planEndYear"
                   :dataArray="stackedData"
-                  :label="`blah`"
+                  :label="`expenses`"
                   :planStartYear="planStartYear"
                   :planEndYear="planEndYear"
                   :birthYear="1981"></stackedBarChart>
         <timeline v-if="!isStackedBarChart && timelineData && timelineData.data.length && planStartYear && planEndYear"
                   :dataArray="timelineData.data"
-                  :label="`blah`"
+                  :label="`expenses`"
                   :planStartYear="planStartYear"
                   :planEndYear="planEndYear"
                   :birthYear="1981"></timeline>
